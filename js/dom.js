e3f68eb84e1b7a65207ff613db121c0c15205a1b
mgstabrani <mgstabrani19@gmail.com>
2021-06-08 10:14:53 +0700

    Add check, trash, and undo button

--- js/dom.js
+++ js/dom.js
@@ -10,7 +10,7 @@ function addBook(){
     const author = document.getElementById("inputBookAuthor").value;
     const year = document.getElementById("inputBookYear").value;
     const isComplete = document.querySelector(".msg").checked;
-    const book = makeBook(id, title, author, year);
+    const book = makeBook("id : " + id, title, "author : " + author, "year : " + year, isComplete);
 
     if(isComplete){
         completeBookshelfList.append(book)
@@ -19,18 +19,22 @@ function addBook(){
     }
 }
 
-function makeBook(id, title, author, year){
+function makeBook(id, title, author, year, isComplete){
     const textJudul = document.createElement("h2");
     textJudul.innerHTML = title;
+    textJudul.className = "title";
 
     const textId = document.createElement("p");
-    textId.innerHTML = "id: " + id;
+    textId.innerHTML = id;
+    textId.className = "id";
 
     const textAuthor = document.createElement("p");
-    textAuthor.innerHTML = "author: " + author;
+    textAuthor.innerHTML = author;
+    textAuthor.className = "author";
 
     const textYear = document.createElement("p");
-    textYear.innerHTML = "year: " + year;
+    textYear.innerHTML = year;
+    textYear.className = "year";
 
     const textContainer = document.createElement("div");
     textContainer.classList.add("inner");
@@ -40,5 +44,70 @@ function makeBook(id, title, author, year){
     container.classList.add("item", "shadow");
     container.append(textContainer);
 
+    if(isComplete){
+        container.append(
+            createTrashButton(),
+            createUndoButton()
+        );
+    } else{
+        container.append(createCheckButton());
+    }
+
     return container;
+}
+
+function createButton(buttonTypeClass , eventListener) {
+    const button = document.createElement("button");
+    button.classList.add(buttonTypeClass);
+    button.addEventListener("click", function (event) {
+        eventListener(event);
+    });
+    return button;
+}
+
+function createCheckButton() {
+    return createButton("check-button", function(event){
+         addBookToCompleted(event.target.parentElement);
+    });
+}
+
+function addBookToCompleted(bookElement) {
+    const textTitle = bookElement.querySelector(".title").innerText;
+    const textId = bookElement.querySelector(".id").innerText;
+    const textAuthor = bookElement.querySelector(".author").innerText;
+    const textYear = bookElement.querySelector(".year").innerText;
+
+    const newBook = makeBook(textId, textTitle, textAuthor, textYear, true);
+    const listCompleted = document.getElementById(COMPLETED_LIST_BOOK_ID);
+    listCompleted.append(newBook);
+    bookElement.remove();
+}
+
+function removeBookFromCompleted(bookElement){
+    bookElement.remove();
+}
+
+function createTrashButton(){
+    return createButton("trash-button", function(event){
+        removeBookFromCompleted(event.target.parentElement);
+    });
+}
+
+function undoBookFromCompleted(bookElement){
+    const listUncompleted = document.getElementById(UNCOMPLETED_LIST_BOOK_ID);
+    
+    const textTitle = bookElement.querySelector(".title").innerText;
+    const textId = bookElement.querySelector(".id").innerText;
+    const textAuthor = bookElement.querySelector(".author").innerText;
+    const textYear = bookElement.querySelector(".year").innerText;
+    
+    const newBook = makeBook(textId, textTitle, textAuthor, textYear, false);
+    listUncompleted.append(newBook);
+    bookElement.remove()
+}
+
+function createUndoButton(){
+    return createButton("undo-button", function(event){
+        undoBookFromCompleted(event.target.parentElement);
+    });
 }
